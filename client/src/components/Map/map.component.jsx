2a0570a6e0e9f6d97a5c2ae48c6451f889544809
Giulio Mario Martena <giuliomariomartena@gmail.com>
2021-09-30 17:38:36 +0200

    Added initial map

--- client/src/components/Map/map.component.jsx
+++ client/src/components/Map/map.component.jsx
@@ -1,12 +1,23 @@
-import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const Map = () => {
   return (
-    <Box>
-      <Typography>This will be the maps component</Typography>
-    </Box>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: 700 }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
