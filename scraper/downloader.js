4c20e7f14098b7395a978b5352bc11f6df440645
Giulio Mario Martena <giuliomariomartena@gmail.com>
2021-09-30 09:52:08 +0200

    Implemented basic downloader

--- scraper/downloader.js
+++ scraper/downloader.js
@@ -1,27 +1,180 @@
 const path = require("path");
 const fs = require("fs");
 const axios = require("axios");
+const cheerio = require("cheerio");
 
-const nycPath = path.join(__dirname, "NYC");
-const londonPath = path.join(__dirname, "London");
-const milanPath = path.join(__dirname, "Milan");
+// Base URL
+const baseUrl = "https://graffiti-database.com";
 
-//console.log(path.join(nycPath, "file" + 1 + ".html"));
+// Paths definition
+const londonDir = path.join(__dirname, "london");
+const milanDir = path.join(__dirname, "milan");
+const nyDir = path.join(__dirname, "nyc");
 
-const nyc = [
-  "https://graffiti-database.com/image/31080",
-  "https://graffiti-database.com/image/31081",
-];
-for (let i = 0; i < nyc.length; i++) {
-  axios
-    .get(nyc[i]) // nyc must be an array of links pertaining to NYC
-    .then((res) => {
-      fs.writeFile(`${path.join(nycPath, i + 1 + ".html")}`, res.data, () => {
-        console.log(`Downloaded file${path.join(nycPath, i + 1 + ".html")}`);
-        setTimeout(() => {
-          console.log("Waiting for new fetch");
-        }, 2500);
-        console.log("Fetching new page");
-      });
-    });
+// Create folders if they don't exist
+if (!fs.existsSync(nyDir)) {
+  fs.mkdirSync(nyDir);
 }
+
+if (!fs.existsSync(londonDir)) {
+  fs.mkdirSync(londonDir);
+}
+
+if (!fs.existsSync(milanDir)) {
+  fs.mkdirSync(milanDir);
+}
+
+// Milan
+const milanLinks = [];
+const milanCategories = [];
+const milanUrl = "https://graffiti-database.com/Italy/Milan";
+axios
+  .get(milanUrl + "/categories")
+  .then((res) => {
+    const $ = cheerio.load(res.data);
+    $("p.image-info > a").each(function () {
+      let i = $(this).attr("href");
+      milanCategories.push(baseUrl + i);
+    });
+  })
+  .then(() => {
+    milanCategories.forEach((link) => {
+      axios
+        .get(link)
+        .then((res) => {
+          const $ = cheerio.load(res.data);
+          $("p.image-info > a").each(function () {
+            let url = $(this).attr("href");
+            milanLinks.push(baseUrl + url);
+          });
+        })
+        .then(() => {
+          milanLinks.forEach((link) => {
+            setInterval(() => {
+              axios.get(link).then((res) => {
+                fs.writeFileSync(
+                  `${londonDir}/${link.split("/")[4]}.html`,
+                  res.data,
+                  (err) => {
+                    if (err) throw err;
+                    console.log("Done");
+                  }
+                );
+              });
+            }, 3000 + Math.random(1000));
+          });
+        })
+        .catch((err) => {
+          console.error(">> Error occured while retrieving graffitis", err);
+        });
+    });
+  })
+  .catch((err) => {
+    console.error(
+      ">> Error occurred while retrieving Milan neighborhoods:",
+      err
+    );
+  });
+
+// New York
+const nyLinks = [];
+const nyCategories = [];
+const nyUrl = "https://graffiti-database.com/USA/New%20York%20City";
+axios
+  .get(nyUrl + "/categories")
+  .then((res) => {
+    const $ = cheerio.load(res.data);
+    $("p.image-info > a").each(function () {
+      let i = $(this).attr("href");
+      nyCategories.push(baseUrl + i);
+    });
+  })
+  .then(() => {
+    //console.log(nyCategories);
+    nyCategories.forEach((link) => {
+      axios
+        .get(link)
+        .then((res) => {
+          const $ = cheerio.load(res.data);
+          $("p.image-info > a").each(function () {
+            let url = $(this).attr("href");
+            nyLinks.push(baseUrl + url);
+          });
+        })
+        .then(() => {
+          nyLinks.forEach((link) => {
+            setInterval(() => {
+              axios.get(link).then((res) => {
+                fs.writeFileSync(
+                  `${nyDir}/${link.split("/")[4]}.html`,
+                  res.data,
+                  (err) => {
+                    if (err) throw err;
+                    console.log("Done");
+                  }
+                );
+              });
+            }, 3000 + Math.random(1000));
+          });
+          //console.log(nyLinks);
+        })
+        .catch((err) => {
+          console.error(">> Error occured while retrieving graffitis", err);
+        });
+    });
+  })
+  .catch((err) => {
+    console.error(">> Error occurred while retrieving NY neighborhoods:", err);
+  });
+
+// London
+const londonLinks = [];
+const londonCategories = [];
+const londonUrl = "https://graffiti-database.com/United%20Kingdom/London";
+axios
+  .get(londonUrl + "/categories")
+  .then((res) => {
+    const $ = cheerio.load(res.data);
+    $("p.image-info > a").each(function () {
+      let i = $(this).attr("href");
+      londonCategories.push(baseUrl + i);
+    });
+  })
+  .then(() => {
+    londonCategories.forEach((link) => {
+      axios
+        .get(link)
+        .then((res) => {
+          const $ = cheerio.load(res.data);
+          $("p.image-info > a").each(function () {
+            let url = $(this).attr("href");
+            londonLinks.push(baseUrl + url);
+          });
+        })
+        .then(() => {
+          londonLinks.forEach((link) => {
+            setInterval(() => {
+              axios.get(link).then((res) => {
+                fs.writeFileSync(
+                  `${londonDir}/${link.split("/")[4]}.html`,
+                  res.data,
+                  (err) => {
+                    if (err) throw err;
+                    console.log("Done");
+                  }
+                );
+              });
+            }, 3000 + Math.random(1000));
+          });
+        })
+        .catch((err) => {
+          console.error(">> Error occured while retrieving graffitis", err);
+        });
+    });
+  })
+  .catch((err) => {
+    console.error(
+      ">> Error occurred while retrieving London neighborhoods:",
+      err
+    );
+  });
