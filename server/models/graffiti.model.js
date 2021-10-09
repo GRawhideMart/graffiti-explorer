f2ce59d8b63ec50aaa7ac8e579402f3f2daebc33
Giulio Mario Martena <giuliomariomartena@gmail.com>
2021-10-09 14:48:47 +0200

    Added support for favs

--- server/models/graffiti.model.js
+++ server/models/graffiti.model.js
@@ -1,23 +1,33 @@
 const { DataTypes } = require("sequelize");
 const db = require("../config/db.config");
 
-const Graffiti = db.define("Graffiti", {
-  // Model attributes are defined here
-  title: {
-    type: DataTypes.STRING(255),
+const Graffiti = db.define(
+  "Graffiti",
+  {
+    // Model attributes are defined here
+    title: {
+      type: DataTypes.STRING(255),
+    },
+    author: {
+      type: DataTypes.STRING(255),
+    },
+    city: {
+      type: DataTypes.STRING(255),
+    },
+    image: {
+      type: DataTypes.STRING(255),
+    },
+    geolocation: {
+      type: DataTypes.GEOMETRY("POINT"),
+    },
+    isFavorite: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
-  author: {
-    type: DataTypes.STRING(255),
-  },
-  city: {
-    type: DataTypes.STRING(255),
-  },
-  image: {
-    type: DataTypes.STRING(255),
-  },
-  geolocation: {
-    type: DataTypes.GEOMETRY("POINT"),
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 module.exports = Graffiti;
