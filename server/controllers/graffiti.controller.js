d9a58914a12a977974e9c5ee139707ebd70299dd
Giulio Mario Martena <giuliomariomartena@gmail.com>
2021-10-09 16:36:56 +0200

    Implemented backend support for favorites

--- server/controllers/graffiti.controller.js
+++ server/controllers/graffiti.controller.js
@@ -3,7 +3,15 @@ const Graffiti = require("../models/graffiti.model");
 // Retrieve all graffitis from the database
 exports.findAll = (req, res) => {
   Graffiti.findAll({
-    attributes: ["id", "title", "author", "city", "image", "geolocation"],
+    attributes: [
+      "id",
+      "title",
+      "author",
+      "city",
+      "image",
+      "geolocation",
+      "isFavorite",
+    ],
   })
     .then((data) => {
       let featureCollection = {
@@ -13,10 +21,12 @@ exports.findAll = (req, res) => {
       featureCollection.features = data.map((entry) => ({
         type: "Feature",
         properties: {
+          id: entry.id,
           title: entry.title,
           author: entry.author,
           city: entry.city,
           image: entry.image,
+          isFavorite: entry.isFavorite,
         },
         geometry: {
           type: entry.geolocation.type,
@@ -33,4 +43,60 @@ exports.findAll = (req, res) => {
           err.message || "Some error occurred while retrieving geolocations."
         )
     );
+};
+
+exports.findById = (req, res, next) => {
+  Graffiti.findByPk(req.params.id, {
+    attributes: [
+      "id",
+      "title",
+      "author",
+      "city",
+      "image",
+      "geolocation",
+      "isFavorite",
+    ],
+  })
+    .then((data) => {
+      let featureCollection = {
+        type: "FeatureCollection",
+        features: [],
+      };
+      featureCollection.features.push({
+        type: "Feature",
+        properties: {
+          id: data.dataValues.id,
+          title: data.dataValues.title,
+          author: data.dataValues.author,
+          city: data.dataValues.city,
+          image: data.dataValues.image,
+          isFavorite: data.dataValues.isFavorite,
+        },
+        geometry: {
+          type: data.dataValues.geolocation.type,
+          coordinates: data.dataValues.geolocation.coordinates,
+        },
+      });
+      return featureCollection;
+    })
+    .then((geoJSON) => res.json(geoJSON))
+    .catch((err) =>
+      res
+        .status(500)
+        .send(
+          err.message || "Some error occurred while retrieving geolocations."
+        )
+    );
+};
+
+exports.toggleFavorite = (req, res, next) => {
+  // must be applied to /api/graffiti/:id
+  Graffiti.update(
+    { isFavorite: req.body.isFavorite },
+    { returning: true, where: { id: req.params.id } }
+  )
+    .then(([rowsUpdate, [updatedGraffiti]]) => {
+      res.json(updatedGraffiti);
+    })
+    .catch(next);
 };
--- server/routes/graffiti.routes.js
+++ server/routes/graffiti.routes.js
@@ -6,5 +6,11 @@ module.exports = (app) => {
   // Retrieve all graffitis
   router.get("/", graffitis.findAll);
 
+  // Retrieve graffiti by ID
+  router.get("/:id", graffitis.findById);
+
+  // Toggle favorite graffiti
+  router.put("/:id", graffitis.toggleFavorite);
+
   app.use("/api/graffiti", router);
 };
